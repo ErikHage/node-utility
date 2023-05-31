f9077ca981d339dec5b8c2e0aa93dc8b729f771d
Erik Hage <ehage4@gmail.com>
2023-05-30 23:53:38 -0400

    Add string expression evaluator

---
+++ test/unit/math/expression/evaluator.spec.js
@@ -0,0 +1,44 @@
+const evaluator = require('../../../../lib/math/expression/evaluator');
+
+const cases = [
+    {
+        input: '10 + 2',
+        expectedResult: 12,
+    },
+    {
+        input: '10 - 2',
+        expectedResult: 8,
+    },
+    {
+        input: '10 * 2',
+        expectedResult: 20,
+    },
+    {
+        input: '10 / 2',
+        expectedResult: 5,
+    },
+    {
+        input: '10 + 2 * 6',
+        expectedResult: 22,
+    },
+    {
+        input: '100 * 2 + 12',
+        expectedResult: 212,
+    },
+    {
+        input: '10 - 2 * 6',
+        expectedResult: -2,
+    },
+    {
+        input: '5 * 10 - 2 * 6 + 100',
+        expectedResult: 138,
+    },
+];
+
+describe('String Expression Evaluator', () => {
+    cases.forEach(c => {
+        it(`when given ${c.input}, expect ${c.expectedResult}`, () => {
+            expect(evaluator.evaluate(c.input)).toEqual(c.expectedResult);
+        });
+    });
+});
